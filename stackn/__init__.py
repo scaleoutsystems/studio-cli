195f860d5f8bf9c1e06897f9d4eadba4fc72d651
Fredrik Wrede <fredrik@scaleoutsystems.com>
2022-12-21 15:27:00 +0100

    Bug/SK-307 | ImportError: module 'stackn' has no attribute 'main' (#4)

---
+++ stackn/__init__.py
@@ -0,0 +1,6 @@
+from .create import create  # noqa: F401
+from .delete import delete  # noqa: F401
+from .get import get  # noqa: F401
+from .login import login  # noqa: F401
+from .main import main  # noqa: F401
+from .set import set  # noqa: F401
